28d36dd4f5cd268423093e1a30a3d064d963ba1f
ipscoder <psawant0015@gmail.com>
2025-03-21 11:51:56 +0530

    modified

--- src/components/layout/RefreshApi.tsx
+++ src/components/layout/RefreshApi.tsx
@@ -1,22 +1,22 @@
-"use client"
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react';
 
-// @ts-expect-error
- const RefreshApi = ({refreshData}) =>{
-    "use client"
-  
-    React.useEffect(() =>{
-      const x = setInterval(() =>{
-        refreshData();
-      },300000)
-      console.log("running")
-  
-     return () => clearInterval(x)
-    },[])
-  
-    return <>
-    <h1>client component</h1>
-    </>
-  }
+// @ts-expect-error: Ignoring type error for now because refreshData is passed dynamically
+const RefreshApi = ({ refreshData }: { refreshData: () => void }) => {
+    useEffect(() => {
+        const x = setInterval(() => {
+            refreshData();
+        }, 300000); // 5 minutes
+        console.log("running");
 
-  export default RefreshApi
+        return () => clearInterval(x);
+    }, [refreshData]); // Add refreshData as a dependency to avoid the warning
+
+    return (
+        <>
+            <h1>Client Component</h1>
+        </>
+    );
+};
+
+export default RefreshApi;
